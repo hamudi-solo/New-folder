d5188a45e648c965ee92b231b16f559d7ed903cf
hamudi-solo <solooozmr@gmail.com>
2025-07-23 02:25:18 -0700

    third commit

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-console.log("hello guys")
-console.error("hellow")
+console.log('hello guys');
+console.error("hellow");
 
-
-
-let name= "mc dev"
-let age="1990"
-
-age = 1995
-const year=2020;
-console.log(age);
+const message = ("mohamed"); 
+console.log(message);
